b3013fbc5e44733c9876bd2d39895fc30f79efb3
Evgenii Popov <axiom777@yandex.ru>
2020-05-01 12:12:27 +0300

    Add owl carousel

--- src/index.js
+++ src/index.js
@@ -5,3 +5,17 @@ import './index.less';
 import './blocks/categories/categories.less';
 import './blocks/specialOffer/specialOffer.less';
 import './blocks/specialOffer/specialOffer';
+
+import 'owl.carousel2/dist/assets/owl.carousel.css';
+import 'owl.carousel2';
+
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel();
+});
+$('.owl-carousel').owlCarousel({
+    margin:10,
+    autoWidth:true,
+    items:4,
+    nav: true,
+    navText: ['<span class="fa fa-angle-left"></span>', '<span class="fa fa-angle-right"></span>']
+})
